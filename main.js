4da2027c9f466c0ba652f1e18a9067bf7ef2d421
R.M.V.Akash Ekanayaka <62983845+vae97@users.noreply.github.com>
2020-12-07 23:26:04 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const electron =require('electron');
 const app =electron.app;
-const path =require('path');
-const url =require('url')
-
 
 const BrowserWindow =electron.BrowserWindow;
 
@@ -10,11 +7,7 @@ var mainWindow;
 
 app.on('ready', function (){
     mainWindow=new BrowserWindow({width:1024, height:769, backgroundColor:'white'});
-    //mainWindow.loadURL('https://sl-covid-app.supunhd96.vercel.app');
-    mainWindow.loadURL(url.format({
-        pathname:path.join(__dirname, 'home.html'),
-        protocol:'file',
-        slashes:true
-    }))
+    mainWindow.loadURL('https://sl-covid-app.supunhd96.vercel.app');
+
 
 });
